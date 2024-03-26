f199a7057216f03a4f59497392da528991108bf2
gmmarsh <gmmarsh@gmail.com>
2024-03-25 21:45:46 -0400

    adding sql query test script

---
+++ crowdfunding_db_query.sql
@@ -0,0 +1,15 @@
+--Query Test
+
+-- query 1: contacts table
+SELECT * FROM contacts;
+
+--query 2: category table
+SELECT * FROM category;
+
+--query 3: subcategory table
+SELECT * FROM subcategory;
+
+--query 4: campaigns table
+SELECT * FROM campaigns;
+
+
